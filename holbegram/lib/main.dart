2512c543e197d028e40cdc8cc3c3acb5a6599fc2
Brayden Vernon <mt.vernon44@gmail.com>
2023-10-31 14:06:04 -0500

    tasks 0-3

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
